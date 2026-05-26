d1b2f27184db755bec5937f4f0343de4044bd690
Aldo Vizcarra <avizcarra87@gmail.com>
2026-05-26 01:06:39 -0700

    Add toggle .is-open to #navMenu

--- js/main.js
+++ js/main.js
@@ -3,4 +3,5 @@ const menu = document.getElementById("navMenu");
 
 toggle.addEventListener("click", (e) => {
   const isOpen = toggle.classList.toggle("is-open");
+  menu.classList.toggle("is-open");
 });
