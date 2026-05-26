4797b340cfac916ea32710c83c7801409e34d3b9
Aldo Vizcarra <avizcarra87@gmail.com>
2026-05-26 01:21:19 -0700

    Edit toggle attributes on toggle

--- js/main.js
+++ js/main.js
@@ -3,5 +3,12 @@ const menu = document.getElementById("navMenu");
 
 toggle.addEventListener("click", (e) => {
   const isOpen = toggle.classList.toggle("is-open");
+
   menu.classList.toggle("is-open");
+
+  toggle.setAttribute("aria-expanded", String(isOpen));
+  toggle.setAttribute(
+    "aria-label",
+    isOpen ? "Close navigation menu" : "Open navigation menu",
+  );
 });
